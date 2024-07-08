6a90f0d47db3feabbfb39b7577744ef6900f0373
Gaius Nale <gaiusnale@gmail.com>
2024-07-08 05:26:36 +0100

    minor bug fix

--- weather_app/urls.py
+++ weather_app/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('', views.weather, name='weather'),
+    path (staticfiles_urlpatterns())
 ]
