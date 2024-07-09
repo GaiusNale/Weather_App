1f95ead4555bad2c423b1cea6635eb0ef44e0ab8
Gaius Nale <gaiusnale@gmail.com>
2024-07-09 15:49:25 +0100

    static file setup

--- weather_project/settings.py
+++ weather_project/settings.py
@@ -123,11 +123,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "staticfiles/"
 
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'weather_app/static'),
-]
 
 STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
 
